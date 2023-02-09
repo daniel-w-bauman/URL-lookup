30f9ad3b5a2d9c7ecf6897db65d59d3a29dfbb11
daniel-w-bauman <daniel.w.bauman@gmail.com>
2023-02-08 20:50:23 -0800

    proof of concept

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-	res.end("<html><body><h1>Hello World</h1></body></html>")
+const bannedUrls = [
+	'virus.com',
+	'verybad.org',
+	'freemoney.scam'
+]
+
+app.get('/v1/urlinfo/:url', (req, res) => {
+	console.log(req.params)
+	if(bannedUrls.includes(req.params.url.toLowerCase().split('/')[0])) {
+		res.end('<h1>banned</h1>')
+	} else {
+		res.end('<h1>allowed</h1>')
+	}
 })
 
 app.listen(3000)
